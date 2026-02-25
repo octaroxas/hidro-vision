18df7c22e3e19a9c101b97a32d7e93fbd65e0f0d
Octacilio Carvalho de Almeida <octa.oca44@gmail.com>
2026-02-25 15:48:38 -0300

    chore(baseurl): Atualiza a url base da api

--- api/Axios.tsx
+++ api/Axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create()
 
-api.defaults.baseURL = 'https://api-mananciais.yuresamarone.shop/api/v1';
+api.defaults.baseURL = 'https://api-mananciais.octacilio.dev.br/api/v1';
 api.defaults.headers.post['Content-Type'] = 'application/json'
 // instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 export default api;
